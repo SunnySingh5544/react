85d1fbf22cd3ef4e90d48ab97ff5b6d275a06095
SunnySingh5544 <166364634+SunnySingh5544@users.noreply.github.com>
2025-05-09 11:11:21 +0530

    Create Interview2.jsx

---
+++ frontend/src/assets/component/Interview2.jsx
@@ -0,0 +1,45 @@
+import { SlCallEnd } from "react-icons/sl";
+import { IoChatbox } from "react-icons/io5";
+import { IoMdCode } from "react-icons/io";
+import { BiFullscreen } from "react-icons/bi";
+
+function Interview() {
+  return (
+    <div className="flex flex-col lg:flex-row p-4">
+     {/* {left div} */}
+      <div className="flex-1">
+        <div className="bg-blue-100 h-60 border rounded-lg mt-4 p-4 flex justify-end sm:h-130">
+          <div className="w-24 h-24 sm:w-32 sm:h-32 bg-white rounded flex items-center justify-center">
+            user
+          </div>
+        </div>
+
+        <div className="bg-blue-800  rounded-lg shadow-lg p-4">
+          <div className="bg-red-300 rounded-lg py-2 px-4">
+            <div className="flex justify-around">
+              <div className="bg-red-700 p-2 rounded text-white">
+                <SlCallEnd />
+              </div>
+              <div className="bg-red-700 p-2 rounded text-white">
+                <IoChatbox />
+              </div>
+              <div className="bg-red-700 p-2 rounded text-white">
+                <IoMdCode />
+              </div>
+              <div className="bg-red-700 p-2 rounded text-white">
+                <BiFullscreen />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+     {/* right div  */}
+      <div className=" h-50 bg-blue-300 border rounded-lg mt-4 lg:mt-4 lg:ml-6 p-4 w-full lg:w-1/3 flex items-center justify-center sm:h-150">
+        HII
+      </div>
+    </div>
+  );
+}
+
+export default Interview;
